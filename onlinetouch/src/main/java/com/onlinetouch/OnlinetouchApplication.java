8b33939c54e0f60e1aeb11110f59992ad3adcf75
ashutosh <ashutoshghimire@gmail.com>
2017-02-17 18:14:42 -0600

    Initial Project Configuration

---
+++ onlinetouch/src/main/java/com/onlinetouch/OnlinetouchApplication.java
@@ -0,0 +1,12 @@
+package com.onlinetouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinetouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinetouchApplication.class, args);
+	}
+}
