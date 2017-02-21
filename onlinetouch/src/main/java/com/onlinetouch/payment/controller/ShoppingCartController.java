60b6e523b56d584a3353977688aa239d084a728c
Manish <Manish@172.17.4.163>
2017-02-21 16:25:09 -0600

    Restful controller

--- onlinetouch/src/main/java/com/onlinetouch/payment/controller/ShoppingCartController.java
+++ onlinetouch/src/main/java/com/onlinetouch/payment/controller/ShoppingCartController.java
@@ -3,6 +3,8 @@ package com.onlinetouch.payment.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,11 +24,11 @@ public class ShoppingCartController {
 	WebUserService webUserService;
 	
 	@RequestMapping(value = "/cartList", method = RequestMethod.GET)
-	public String getCartList(Model model) {
+	public ResponseEntity<String> getCartList(Model model) {
 		List<ShoppingCart> shoppingCart = shoppingCartService.getAll();
 		model.addAttribute("shoppingCarts", shoppingCart);
 		double totalPrice = shoppingCartService.findTotalCartCost(shoppingCart);
 		model.addAttribute("totalPrice", totalPrice);
-		return "cartList";
+		return new ResponseEntity<String>("cartList", HttpStatus.OK);
 	}
 }
