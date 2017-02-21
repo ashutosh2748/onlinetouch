6906d3fb668ecfd47595cd4395ce4ad859ab3f55
ashutosh <ashutoshghimire@gmail.com>
2017-02-21 17:35:39 -0600

    data-init.sql rename and update your database

--- onlinetouch/src/main/java/com/onlinetouch/inventory/entity/CategoryLeaf.java
+++ onlinetouch/src/main/java/com/onlinetouch/inventory/entity/CategoryLeaf.java
@@ -7,23 +7,23 @@ import javax.persistence.OneToMany;
 
 @Entity
 public class CategoryLeaf extends Category {
-	@OneToMany(mappedBy="categoryleaf")
-	List<Product> products;
+	//@OneToMany(mappedBy="categoryleaf")
+	//List<Product> products;
 	CategoryLeaf(){
 		hasproduct=true;
 	}
-	public List<Product> getProduct() {
-		return products;
-	}
-	public void setProduct(List<Product> product) {
-		this.products = product;
-	}
-	public void addProduct(Product product) {
-		products.add(product);
-	}
-	public void removeProduct(Product product) {
-		products.remove(product);
-	}
+	//public List<Product> getProduct() {
+	//	return products;
+	//}
+	//public void setProduct(List<Product> product) {
+	//	this.products = product;
+	//}
+	//public void addProduct(Product product) {
+	//	products.add(product);
+	//}
+	//public void removeProduct(Product product) {
+	//	products.remove(product);
+	//}
 	@Override
 	public List<Category> getChildCategories() {
 		// TODO Auto-generated method stub
---
+++ onlinetouch/src/main/resources/data-init.sql
@@ -0,0 +1,99 @@
+
+use cs544;
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (1, 'South Jakaylaburgh', 'NorthCarolina', '60615 Althea Creek Apt. 367', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (2, 'Lake Arlie', 'NorthDakota', '1105 Roberto Fords Suite 237', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (3, 'Mantemouth', 'District of Columbia', '701 Ara Fort', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (4, 'East Brady', 'Minnesota', '511 Tillman Mountains', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (5, 'Cassandramouth', 'Maine', '51851 Marquardt Run Apt. 111', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (6, 'West Felixfurt', 'Ohio', '10815 Osinski Route', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (7, 'Natashaberg', 'Maryland', '2686 Jarred Trail Suite 867', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (8, 'New Tanya', 'Oregon', '8298 Witting Freeway Suite 425', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (9, 'Goodwinfort', 'Kansas', '7229 Koss Extensions', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (10, 'East Victoriaside', 'Wyoming', '509 Becker Mountain Suite 380', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (11, 'Crystelton', 'Mississippi', '8045 Kertzmann Land Suite 082', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (12, 'Nadertown', 'Florida', '3150 Colleen Fall', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (13, 'Skilesborough', 'Wisconsin', '1316 Armstrong Manor Suite 830', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (14, 'Ritabury', 'District of Columbia', '684 Stark Cape Apt. 622', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (15, 'Everardomouth', 'Delaware', '63496 Owen Well Suite 865', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (16, 'Krisstad', 'WestVirginia', '3612 Alexanne Trail Apt. 319', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (17, 'Jasperstad', 'Arkansas', '4128 Vance Street Suite 669', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (18, 'New Weldon', 'Texas', '0587 Janick Terrace Apt. 892', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (19, 'Kerlukeburgh', 'Kansas', '9336 Rosenbaum Mill', '');
+INSERT INTO address (`id`, `city`, `state`, `street`, `zip`) VALUES (20, 'Hodkiewiczbury', 'Arkansas', '438 Corine Lake Apt. 002', '');
+
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CompositeCategory', 1,1, 'tenetur');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CategoryLeaf', 2,0, 'tempora');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CompositeCategory', 3,0, 'dolores');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CategoryLeaf', 4,0, 'similique');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CompositeCategory', 5,1, 'veritatis');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CompositeCategory', 6,0, 'rem');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CompositeCategory', 7,1, 'et');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CompositeCategory', 8,1, 'a');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CategoryLeaf', 9,0, 'temporibus');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CompositeCategory', 10,1, 'molestiae');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CategoryLeaf', 11,1, 'sit');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CompositeCategory', 12,0, 'dolorem');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CompositeCategory', 13,1, 'non');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CategoryLeaf', 14,0, 'ex');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CompositeCategory', 15,0, 'amet');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CategoryLeaf', 16,1, 'ipsum');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CompositeCategory', 17,0, 'velit');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CompositeCategory', 18,1, 'quod');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CategoryLeaf', 19,1, 'reiciendis');
+INSERT INTO category (`dtype`, `id`, `hasproduct`, `name`) VALUES ('CategoryLeaf', 20,1, 'aut');
+
+
+
+INSERT INTO brand (`id`, `name`) VALUES (1, 'quis');
+INSERT INTO brand (`id`, `name`) VALUES (2, 'cupiditate');
+INSERT INTO brand (`id`, `name`) VALUES (3, 'et');
+INSERT INTO brand (`id`, `name`) VALUES (4, 'consectetur');
+INSERT INTO brand (`id`, `name`) VALUES (5, 'veniam');
+INSERT INTO brand (`id`, `name`) VALUES (6, 'in');
+INSERT INTO brand (`id`, `name`) VALUES (7, 'et');
+INSERT INTO brand (`id`, `name`) VALUES (8, 'sit');
+INSERT INTO brand (`id`, `name`) VALUES (9, 'recusandae');
+INSERT INTO brand (`id`, `name`) VALUES (10, 'aut');
+INSERT INTO brand (`id`, `name`) VALUES (11, 'earum');
+INSERT INTO brand (`id`, `name`) VALUES (12, 'asperiores');
+INSERT INTO brand (`id`, `name`) VALUES (13, 'suscipit');
+INSERT INTO brand (`id`, `name`) VALUES (14, 'amet');
+INSERT INTO brand (`id`, `name`) VALUES (15, 'commodi');
+INSERT INTO brand (`id`, `name`) VALUES (16, 'dolores');
+INSERT INTO brand (`id`, `name`) VALUES (17, 'atque');
+INSERT INTO brand (`id`, `name`) VALUES (18, 'et');
+INSERT INTO brand (`id`, `name`) VALUES (19, 'fugit');
+INSERT INTO brand (`id`, `name`) VALUES (20, 'ipsum');
+
+
+
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (1, 'Reprehenderit repudiandae explicabo enim fugiat dolores enim. Aperiam quas ipsum saepe non. Temporibus ut nemo quod. Porro facilis eum cumque repudiandae.', 'http://lorempixel.com/400/300/sports/', '0.00', 'Ducimus itaque et excepturi esse.', 14, 3);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (2, 'Culpa maiores nihil a id mollitia quisquam. Ea dignissimos quo nemo minima et. Totam sequi illum sequi voluptas.', 'http://lorempixel.com/400/300/sports/', '2685453.00', 'Neque eligendi error possimus in dolorem.', 16, 17);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (3, 'Quo ipsa nesciunt id repellendus pariatur pariatur. Recusandae possimus expedita ratione quaerat cum blanditiis placeat. Omnis quas rerum sit aut ipsum in praesentium. Nihil eius repellat natus. Modi unde quo tenetur tempore.', 'http://lorempixel.com/400/300/sports/', '1.00', 'Est quam hic doloribus laudantium voluptas placeat aut.', 6, 6);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (4, 'Ipsa ipsa et iste quia repellendus animi aperiam dolore. Magni quia dolorum et natus perspiciatis. Distinctio pariatur est deleniti placeat est fuga sint ad. Et ut est est hic iure veritatis.', 'http://lorempixel.com/400/300/sports/', '2.83', 'Et reiciendis ipsa quia eaque repudiandae vero ullam.', 7, 20);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (5, 'Dicta omnis totam ut atque quis laudantium. Voluptatem natus blanditiis saepe quia odio qui earum. At ratione corporis ut repellat doloremque ex magnam.', 'http://lorempixel.com/400/300/sports/', '417469.90', 'Sint neque adipisci et quisquam neque numquam maiores.', 7, 19);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (6, 'Consequuntur doloremque sed adipisci. Accusamus laboriosam eos incidunt eos ea adipisci quisquam. Error et odit omnis velit veniam sapiente est mollitia.', 'http://lorempixel.com/400/300/sports/', '79561222.00', 'Doloremque sint qui reiciendis.', 16, 3);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (7, 'Unde amet quae molestiae repellat nihil. Adipisci illum saepe velit consequatur sed voluptas odio. Ab animi dolorum doloribus porro saepe consequatur. Est adipisci dolore facilis nesciunt facilis esse qui.', 'http://lorempixel.com/400/300/sports/', '7.12', 'Ut ab sit necessitatibus magni odio eius debitis.', 12, 12);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (8, 'Quia sed quo veritatis. Eos quia qui vitae. Non odit qui non nostrum rerum aut quo. Est ut exercitationem eum quibusdam nihil sit.', 'http://lorempixel.com/400/300/sports/', '8699.05', 'Aut sint ut nostrum.', 6, 8);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (9, 'Omnis et et et et. Unde ut cumque est et. Expedita quia qui dolores a autem ab.', 'http://lorempixel.com/400/300/sports/', '5743054.82', 'Ea velit esse nobis in odio qui id.', 7, 7);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (10, 'Voluptas aut id ullam cum laboriosam quia. Tempore aut aliquam ab saepe tempore. Architecto vel pariatur autem. Occaecati quis in fuga quis cumque voluptas. Quas sunt corporis nihil nobis.', 'http://lorempixel.com/400/300/sports/', '0.00', 'Neque aut incidunt voluptas blanditiis perspiciatis aspernatur voluptates vero.', 15, 12);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (11, 'Et inventore odit culpa placeat. Rerum rerum et aut nihil ab et consequatur. Cupiditate dolor dignissimos et sed molestiae. Fuga nemo aut facere.', 'http://lorempixel.com/400/300/sports/', '3.31', 'Quidem fugit quia iure et aut atque.', 9, 3);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (12, 'Quia maiores eligendi vel dolorem dignissimos dolor. Placeat corrupti quaerat aut quisquam inventore labore. Placeat libero illum et qui enim aut. Omnis ea enim ex voluptas cupiditate eveniet.', 'http://lorempixel.com/400/300/sports/', '3386038.00', 'Explicabo aut praesentium sequi facere reiciendis.', 8, 11);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (13, 'Porro eum quasi facilis ea. Molestiae sunt ut doloribus in distinctio soluta repellat qui. Quis rerum nihil rerum sed est error.', 'http://lorempixel.com/400/300/sports/', '811.56', 'Sunt ea eum a sint esse dolorem ex.', 14, 4);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (14, 'Eum qui sit quae debitis placeat et praesentium. Nesciunt ad voluptatem suscipit voluptatem ut suscipit illum.', 'http://lorempixel.com/400/300/sports/', '3188.57', 'Nostrum sapiente esse incidunt voluptatem.', 12, 10);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (15, 'Excepturi libero quo fuga ad temporibus voluptatem minus. Quos saepe rerum et architecto error libero et quos. Ut repellendus illo consequatur ipsum libero nisi.', 'http://lorempixel.com/400/300/sports/', '3.82', 'Est voluptatum fugiat harum libero rerum.', 16, 2);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (16, 'Tempore quisquam sit eaque unde alias quo fugiat repellendus. Ipsam eum porro enim illo quaerat omnis.', 'http://lorempixel.com/400/300/sports/', '0.00', 'Omnis distinctio non dignissimos aliquid voluptatem.', 8, 10);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (17, 'Molestiae dolor aut minus. Possimus aut nam qui iusto maiores facilis. Voluptas ut earum dicta.', 'http://lorempixel.com/400/300/sports/', '8529195.11', 'Facilis praesentium illo blanditiis rem.', 4, 4);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (18, 'Consequatur quam sunt a magnam aut cupiditate. Incidunt temporibus sit est error reiciendis deleniti. Officiis et laborum inventore.', 'http://lorempixel.com/400/300/sports/', '0.00', 'Et corporis qui et sed.', 6, 9);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (19, 'Quis cumque nostrum eligendi. Inventore similique facere dignissimos eaque. Cumque similique dolores quibusdam odio et pariatur. Sed ipsum quo nihil est voluptates laudantium.', 'http://lorempixel.com/400/300/sports/', '28149.00', 'Maiores est aperiam ut enim optio numquam quo.', 9, 13);
+INSERT INTO product (`id`, `description`, `img_url`, `sellprice`, `title`, `brand_id`, `categoryleaf_id`) VALUES (20, 'Consectetur ut deleniti quidem saepe voluptatem esse temporibus. Saepe laudantium recusandae quia quidem corporis suscipit amet. Quibusdam et quia iusto quis non similique. Quos iure harum voluptas aut ullam.', 'http://lorempixel.com/400/300/sports/', '3117.00', 'Fugit itaque et numquam sequi expedita possimus.', 8, 16);
+
+INSERT INTO `cs544`.`childcategories` (`composite_category_id`, `child_categories_id`) VALUES ('1', '2');
+INSERT INTO `cs544`.`childcategories` (`composite_category_id`, `child_categories_id`) VALUES ('1', '3');
+INSERT INTO `cs544`.`childcategories` (`composite_category_id`, `child_categories_id`) VALUES ('3', '4');
+INSERT INTO `cs544`.`childcategories` (`composite_category_id`, `child_categories_id`) VALUES ('3', '5');
+
+
+INSERT INTO `cs544`.`web_user` (`id`, `email`, `enabled`, `first_name`, `last_name`, `middle_name`, `password`, `user_name`) VALUES ('1', 'sdf', '1', 'sdf', 'sdf', 'sdf', '$2a$10$4eqIF5s/ewJwHK1p8lqlFOEm2QIA0S8g6./Lok.pQxqcxaBZYChRm', 'abc125');
+INSERT INTO `cs544`.`user_roles` (`user_role_id`, `role`) VALUES ('1', 'ROLE_ADMIN');
+INSERT INTO `cs544`.`web_user_roles` (`user_id`, `role_id`) VALUES ('1', '1');
