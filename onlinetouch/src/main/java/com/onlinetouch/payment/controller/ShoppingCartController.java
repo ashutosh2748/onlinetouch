964846925c484e7ace9d167918386274a5c64e9a
Manish <Manish@172.17.4.163>
2017-02-21 22:06:24 -0600

    template ui configuration

--- onlinetouch/src/main/java/com/onlinetouch/payment/controller/ShoppingCartController.java
+++ onlinetouch/src/main/java/com/onlinetouch/payment/controller/ShoppingCartController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+//import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,12 +27,41 @@ public class ShoppingCartController {
 	@Autowired
 	WebUserService webUserService;
 	
-	@RequestMapping(value = "/cartList", method = RequestMethod.GET)
+	@RequestMapping(value = "/carts",
+			method = RequestMethod.GET)
 	public ResponseEntity<String> getCartList(Model model) {
 		List<ShoppingCart> shoppingCart = shoppingCartService.getAll();
+		if(shoppingCart == null)
+			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
 		model.addAttribute("shoppingCarts", shoppingCart);
 		double totalPrice = shoppingCartService.findTotalCartCost(shoppingCart);
 		model.addAttribute("totalPrice", totalPrice);
-		return new ResponseEntity<String>("cartList", HttpStatus.OK);
+		return new ResponseEntity<String>("carts", HttpStatus.OK);
 	}
+	
+	
+	  //------------------- Update a Cart --------------------------------------------------------
+    
+    @RequestMapping(value = "/shoppingCart/{productId}",
+    		method = RequestMethod.PUT,
+    		consumes = MediaType.APPLICATION_JSON_VALUE,
+    		produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ShoppingCart> updateUser(@PathVariable("productId") long id, @RequestBody ShoppingCart cart) {
+        System.out.println("Updating cart " + id);
+          
+        ShoppingCart currentCart = shoppingCartService.findByOrderId(id);
+          
+        if (currentCart == null) {
+            System.out.println("Cart with id " + id + " not found");
+            return new ResponseEntity<ShoppingCart>(HttpStatus.NOT_FOUND);
+        }
+  
+//        currentCart.setOrder(cart.getOrder());;
+//        currentCart.setAddedDate(cart.getAddedDate());;
+//        currentCart.setQuantity(cart.getQuantity());
+          
+        shoppingCartService.saveShoppingCart(id);
+        return new ResponseEntity<ShoppingCart>(currentCart, HttpStatus.NO_CONTENT);
+    }
+	
 }
--- onlinetouch/src/main/webapp/static/scripts/app.js
+++ onlinetouch/src/main/webapp/static/scripts/app.js
@@ -1,1 +1,70 @@
 'use strict';
+angular.module('onlineTouch', ['ui.router','abc'])
+.config(function($stateProvider, $urlRouterProvider) {
+         $stateProvider
+        
+            // route for the home page
+            .state('app', {
+                url:'/',
+                views: {
+                    'header': {
+                        templateUrl : '../WEB-INF/views/header.html',
+                    },
+                    'content': {
+                        templateUrl : '../WEB-INF/views/home.html',
+                        controller  : 'IndexController'
+                    },
+                    'footer': {
+                        templateUrl : '../WEB-INF/views/footer.html',
+                    }
+                }
+
+            })
+        
+            // route for the aboutus page
+            .state('app.aboutus', {
+                url:'aboutus',
+                views: {
+                    'content@': {
+                        templateUrl : '../WEB-INF/views/aboutus.html',
+                        controller  : 'AboutController'                  
+                    }
+                }
+            })
+        
+            // route for the contactus page
+            .state('app.contactus', {
+                url:'contactus',
+                views: {
+                    'content@': {
+                        templateUrl : '../WEB-INF/views/contactus.html',
+                        controller  : 'ContactController'                  
+                    }
+                }
+            })
+
+            // route for the menu page
+            .state('app.menu', {
+                url: 'menu',
+                views: {
+                    'content@': {
+                        templateUrl : '../WEB-INF/views/menu.html',
+                        controller  : 'MenuController'
+                    }
+                }
+            })
+
+            // route for the cartsdetail page
+            .state('app.dishdetails', {
+                url: 'menu/:id',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/dishdetail.html',
+                        controller  : 'DishDetailController'
+                   }
+                }
+            });
+    
+        $urlRouterProvider.otherwise('/');
+    })
+;
