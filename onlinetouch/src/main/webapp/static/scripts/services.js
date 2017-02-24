1c774f2de5337c156751f6e67abfd37635e8232d
Manish <Manish@172.17.4.163>
2017-02-23 18:10:31 -0600

    services for products

---
+++ onlinetouch/src/main/webapp/static/scripts/services.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('onlineTouch')
+        .constant("baseURL","http://localhost:8080/")
+        .service('productFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+
+            this.getProducts = function(){
+                return $resource(baseURL+"product/:id",null,  {'update':{method:'PUT' }});
+                };
+              //promotional products  
+            this.getPromotions = function(){
+                return $resource(baseURL+"promotions/:id",null,  {'update':{method:'PUT' }});
+                };
+
+        }])
+;
+
+angular.module('onlineTouch')
+        .factory('corporateFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+
+            var corpfac = {};
+            corpfac.getLeaders = function(){
+                return $resource(baseURL+"leadership/:id",null,  {'update':{method:'PUT' }});
+                };
+
+            return corpfac;
+    }])
+
+;
+
+angular.module('onlineTouch')
+        .factory('feedbackFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+
+            var feedbackfac = {};
+            feedbackfac.sendFeedback = function(){
+                return $resource(baseURL+"feedback/:id",null,  {'update':{method:'PUT' }});
+                };
+
+            return feedbackfac;
+    }])
+
+;
