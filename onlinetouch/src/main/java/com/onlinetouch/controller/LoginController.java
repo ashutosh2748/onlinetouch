a0dfd6da5f4d745abc8f313a566da2a486364a44
krishna1bhat <krishna1bhat@localhost>
2017-02-24 21:51:31 -0600

    Custom login submit

--- onlinetouch/src/main/java/com/onlinetouch/controller/LoginController.java
+++ onlinetouch/src/main/java/com/onlinetouch/controller/LoginController.java
@@ -1,8 +1,18 @@
 package com.onlinetouch.controller;
 
 import java.security.Principal;
+import java.util.Collection;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +26,32 @@ public class LoginController {
 	 * */
 	public Principal user(Principal user) {
 		return user;
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ResponseEntity<String> login(@Valid @RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout, Model model, Authentication authentication) {
+		
+		String message = determineTargetUrl(authentication);
+		if(message != null){
+			model.addAttribute("redirectView", message);
+			return new ResponseEntity<String>("user", HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+	}
+	
+	protected String determineTargetUrl(Authentication authentication) {
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		for (GrantedAuthority grantedAuthority : authorities) {
+			if (grantedAuthority.getAuthority().equals("ROLE_CUSTOMER")) {
+				return "customer";
+			} else if (grantedAuthority.getAuthority().equals("ROLE_ADMIN")) {
+				return "admin";
+			} else if (grantedAuthority.getAuthority().equals("ROLE_INVENTORY_MANAGER")) {
+				return "inventory";
+			}
+		}
+		
+		return null;	//if user not found
 	}
 }
